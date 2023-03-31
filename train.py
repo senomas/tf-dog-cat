1aa7e2f6e3c8d912c61ade4005e0efad63061d22
denmaseno <agus@hanoman.co.id>
2023-03-31 13:20:42 +0700

    model 2 - 75 val accuracy

--- train.py
+++ train.py
@@ -1,10 +1,11 @@
 import os
 
+import tensorflow as tf
 from tensorflow import keras
 from tensorflow import train
-from keras.callbacks import ModelCheckpoint
-from keras import layers
-from keras import preprocessing
+from tensorflow.keras.callbacks import ModelCheckpoint
+from tensorflow.keras import layers
+from tensorflow.keras import preprocessing
 from tensorflow.keras import optimizers
 
 image_w = 200
@@ -12,20 +13,28 @@ image_h = 200
 
 # define cnn model
 def define_model():
-    model = keras.Sequential()
-    model.add(layers.Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same', input_shape=(image_w, image_h, 3)))
-    model.add(layers.MaxPooling2D((2, 2)))
-    model.add(layers.Flatten())
-    model.add(layers.Dense(128, activation='relu', kernel_initializer='he_uniform'))
-    model.add(layers.Dense(1, activation='sigmoid'))
+    model = keras.Sequential([
+        layers.experimental.preprocessing.Rescaling(1./255),
+        layers.Conv2D(32, 3, activation='relu'),
+        layers.MaxPooling2D(),
+        layers.Conv2D(32, 3, activation='relu'),
+        layers.MaxPooling2D(),
+        layers.Conv2D(64, 3, activation='relu'),
+        layers.MaxPooling2D(),
+        layers.Flatten(),
+        layers.Dense(128, activation='relu'),
+        layers.Dense(1, activation='sigmoid')
+    ])
     # compile model
-    opt = optimizers.legacy.SGD(learning_rate=0.001, momentum=0.9)
-    model.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accuracy'])
+    model.compile(
+        optimizer='adam',
+        loss=tf.losses.BinaryCrossentropy(),
+        metrics=['accuracy']) 
     return model
 
 model = define_model()
 
-checkpoint_path = "training_checkpoint/cp.ckpt"
+checkpoint_path = "checkpoint/cp.ckpt"
 checkpoint_dir = os.path.dirname(checkpoint_path)
 
 latest = train.latest_checkpoint(checkpoint_dir)
@@ -33,26 +42,38 @@ if latest is not None:
     print(f'LOAD CHECKPOINT {latest}')
     model.load_weights(latest)
 
-print(f'MODEL INIT {model.summary()}')
-
 # Create a callback that saves the model's weights
 cp_callback = ModelCheckpoint(filepath=checkpoint_path, save_weights_only=True, verbose=1)
 
-# create data generator
-datagen = preprocessing.image.ImageDataGenerator(rescale=1.0/255.0)
+# prepare datasource
+train_ds = preprocessing.image_dataset_from_directory(
+    'data/train-sample/',
+    validation_split=0.2,
+    subset='training',
+    seed=123,
+    image_size=(image_w, image_h),
+    batch_size=32
+)
 
-# prepare iterators
-print('training...')
-train_it = datagen.flow_from_directory('data/train-sample/', class_mode='binary', batch_size=64, target_size=(image_w, image_h))
-print('testing...')
-test_it = datagen.flow_from_directory('data/test-sample/', class_mode='binary', batch_size=64, target_size=(image_w, image_h))
+val_ds = preprocessing.image_dataset_from_directory(
+    'data/test-sample/',
+    validation_split=0.2,
+    subset='validation',
+    seed=123,
+    image_size=(image_w, image_h),
+    batch_size=32
+)
 
 # fit model
-print('model.fit')
-history = model.fit(train_it, steps_per_epoch=len(train_it), validation_data=test_it, validation_steps=len(test_it), epochs=40, callbacks=[cp_callback], verbose=2)
+model.fit(
+    train_ds,
+    validation_data=val_ds,
+    epochs=40,
+    callbacks=[cp_callback], verbose=2
+)
 print(f'MODEL SUMMARY {model.summary()}')
+model.save('model/model_1')
 
 # evaluate model
-print('model.evaluate')
-_, acc = model.evaluate(test_it, steps=len(test_it), verbose=0)
+_, acc = model.evaluate(val_ds, verbose=0)
 print('> %.3f' % (acc * 100.0))
